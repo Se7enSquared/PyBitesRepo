4df8b18401f4330bd681fc3eb8ab6a55a4f0548e
Heather Gray <heathergray153@gmail.com>
2022-03-26 12:28:23 -0600

    'submission Bite 16 @ codechalleng.es'

---
+++ 16/gendates.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+from itertools import islice
+
+PYBITES_BORN = datetime(year=2016, month=12, day=19)
+
+
+def gen_special_pybites_dates():
+    born = PYBITES_BORN
+    while True:
+        yield born + timedelta(days=100)
+        born = born + timedelta(days=100)
+
+# gen = gen_special_pybites_dates()
+# print(list(islice(gen, 10)))
