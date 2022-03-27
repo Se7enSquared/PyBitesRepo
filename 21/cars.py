281910edc1b1fd8315a87e26bf6230f09dee7ffe
Heather Gray <heathergray153@gmail.com>
2022-03-27 11:21:49 -0600

    'submission Bite 21 @ codechalleng.es'

--- 21/cars.py
+++ 21/cars.py
@@ -1,4 +1,5 @@
 from typing import Dict, List
+import re
 
 cars = {
     'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
@@ -16,8 +17,7 @@ def get_all_jeeps(cars: CarsType = cars) -> str:
     Retrieve the 'Jeep' models from the cars dict and join them by a
     comma and space (', '). Leave the original ordering intact.
     """
-    pass
-
+    return ', '.join(cars['Jeep'])
 
 def get_first_model_each_manufacturer(cars: CarsType = cars) -> List[str]:
     """
@@ -25,7 +25,7 @@ def get_first_model_each_manufacturer(cars: CarsType = cars) -> List[str]:
     manufacturer. Return the matching models in a list leaving the original
     ordering intact.
     """
-    pass
+    return [model[0] for model in cars.values()]
 
 
 def get_all_matching_models(
@@ -36,7 +36,13 @@ def get_all_matching_models(
     'grep' string which defaults to DEFAULT_SEARCH ('trail').
     Sort the resulting sequence alphabetically
     """
-    pass
+    models = []
+    grep = grep.lower()
+    for lst in cars.values():
+        for model in lst:
+            if model.lower().find(grep) != -1:
+                models.append(model)
+    return sorted(models)
 
 
 def sort_car_models(cars: CarsType = cars) -> CarsType:
@@ -44,4 +50,7 @@ def sort_car_models(cars: CarsType = cars) -> CarsType:
     Loop through the cars dict returning a new dict with the
     same keys and the values sorted alphabetically.
     """
-    pass
+    new_dict = {}
+    for car, model in cars.items():
+        new_dict[car] = sorted(model)
+    return new_dict
