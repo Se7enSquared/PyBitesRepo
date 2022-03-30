f49ba3d563e2d0a3df34b0c87cf50ccc075ffc6b
Heather Gray <heathergray153@gmail.com>
2022-03-29 20:15:03 -0600

    'submission Bite 44 @ codechalleng.es'

---
+++ 44/license.py
@@ -0,0 +1,22 @@
+import string
+import secrets
+
+
+def gen_key(parts: int = 4, chars_per_part: int = 8) -> str:
+    """
+    Generate and return a random license key containing
+    upper case characters and digits.
+
+    Example with default "parts" and "chars_per_part"
+    being 4 and 8: KI80OMZ7-5OGYC1AC-735LDPT1-4L11XU1U
+
+    If parts = 3 and chars_per_part = 4 a random license
+    key would look like this: 54N8-I70K-2JZ7
+    """
+
+    alphabet = string.ascii_uppercase + string.digits
+    password = ''
+    for i in range(parts):
+        password += ''.join(secrets.choice(alphabet) for i in range(chars_per_part))
+        password += '-'
+    return password[:-1]
