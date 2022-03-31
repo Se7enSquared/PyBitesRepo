c3fe9c380e536a1b1ba576e9cfd6e8c8d52e48e0
Heather Gray <heathergray153@gmail.com>
2022-03-30 19:03:12 -0600

    'submission Bite 46 @ codechalleng.es'

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -2,6 +2,8 @@ from typing import Union
 
 
 def fizzbuzz(num: int) -> Union[str, int]:
+    if num % 3 == 0 and num % 5 == 0:
+        return 'Fizz Buzz'
     if num % 3 == 0:
         return 'Fizz'
     elif num % 5 == 0:
