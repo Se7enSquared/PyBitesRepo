1a853fb065bad52f0eaccb8714b3263802abdd23
Heather Gray <heathergray153@gmail.com>
2022-04-02 10:20:47 -0600

    'submission Bite 83 @ codechalleng.es'

--- 83/timezone.py
+++ 83/timezone.py
@@ -7,7 +7,6 @@ SPAIN = timezone("Europe/Madrid")
 def what_time_lives_pybites(naive_utc_dt):
     """Receives a naive UTC datetime object and returns a two element
     tuple of Australian and Spanish (timezone aware) datetimes"""
-    utc_tz = utc
-    aus = utc_tz.localize(naive_utc_dt)
-    esp = utc_tz.localize(naive_utc_dt)
-    return(aus.astimezone(AUSTRALIA), esp.astimezone(SPAIN))
+    aus = utc.localize(naive_utc_dt).astimezone(AUSTRALIA)
+    esp = utc.localize(naive_utc_dt).astimezone(SPAIN)
+    return (aus, esp)
