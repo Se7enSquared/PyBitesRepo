cc6abbe9d8edcf0b7b2b9bda036366c5cae2e758
Heather Gray <heathergray153@gmail.com>
2022-04-02 13:50:34 -0600

    'submission Bite 115 @ codechalleng.es'

--- 115/indents.py
+++ 115/indents.py
@@ -3,6 +3,4 @@ def count_indents(text: str) -> int:
     """
     Count and return the number of leading white space characters (' ').
     """
-    if '\t' in text:
-        return 0
-    return len(text) - len(text.lstrip())
+    return len(text) - len(text.lstrip(' '))
