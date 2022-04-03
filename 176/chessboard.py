4fcb90661e09fdd1ce7ab8c33c6eda319d003b35
Heather Gray <heathergray153@gmail.com>
2022-04-03 15:56:57 -0600

    'submission Bite 176 @ codechalleng.es'

---
+++ 176/chessboard.py
@@ -0,0 +1,12 @@
+WHITE, BLACK = ' ', '#'
+
+
+def create_chessboard(size=8):
+    """Create a chessboard with of the size passed in.
+       Don't return anything, print the output to stdout"""
+    board_row = ''
+    for i in range(size):
+        if i % 2 == 0:
+            print(size//2 * (WHITE + BLACK))
+        else:
+            print(size//2 * (BLACK + WHITE))
