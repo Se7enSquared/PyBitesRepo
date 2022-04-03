e093de7672cdfb25c86d7d1d3c21ab3f2669beb2
Heather Gray <heathergray153@gmail.com>
2022-04-03 16:39:22 -0600

    'submission Bite 180 @ codechalleng.es'

---
+++ 180/names.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    parsed_data = [line for line in data.split('\n')[1:]]
+    for line in parsed_data:
+        last, first, country = line.split(',')
+        countries[country] += [first + ' ' + last]
+    return countries
