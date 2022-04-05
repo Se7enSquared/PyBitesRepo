91015347dbc4b4a169ee74859eece13fff474fcf
Heather Gray <heathergray153@gmail.com>
2022-04-04 19:34:18 -0600

    'submission Bite 209 @ codechalleng.es'

---
+++ 209/docstring.py
@@ -0,0 +1,11 @@
+def sum_numbers(numbers):
+    """Sums numbers
+
+    :param numbers: list of numbers
+    :type numbers: list
+    ...
+    :raises TypeError: if not all numeric values passed in
+    ...
+    :rtype: int
+    """
+    pass
