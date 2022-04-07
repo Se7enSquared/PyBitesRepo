9cbbabeef6dd6546883210ee78f0cd200a581858
Heather Gray <heathergray153@gmail.com>
2022-04-06 18:11:52 -0600

    'submission Bite 238 @ codechalleng.es'

--- 238/test_fibonacci.py
+++ 238/test_fibonacci.py
@@ -1,14 +1,20 @@
 from fibonacci import fib
 import pytest
+
 # write one or more pytest functions below, they need to start with test_
 def test_n_less_than_zero():
     with pytest.raises(ValueError):
         fib(-1)
 
+
 def test_n_0_1():
-    assert(fib(0), 0)
-    assert(fib(1), 1)
+    assert (fib(0), 0)
+    assert (fib(1), 1)
+
 
 def test_fib():
-    assert(fib(3), [1,1,2,3])
-    assert(fib(13), [1,1,2,3,5,8,13])
+    assert (fib(3), (1, 1, 2, 3))
+    assert (fib(13), (1, 1, 2, 3, 5, 8, 13))
+    assert (fib(34), (1, 1, 2, 3, 5, 8, 13, 21, 34))
+
+
