03d4ea62a33a5089cb5ca0cc46727073ecf78aab
Heather Gray <heathergray153@gmail.com>
2022-04-07 18:07:03 -0600

    'submission Bite 238 @ codechalleng.es'

--- 238/fibonacci.py
+++ 238/fibonacci.py
@@ -1,7 +1,7 @@
 def fib(n):
-   if n < 0:
+    if n < 0:
         raise ValueError
-   elif n in (0, 1):
-       return n
-   else:
-       return(fib(n-1) + fib(n-2))
+    elif n in (0, 1):
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
