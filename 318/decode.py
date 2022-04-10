f8e116d1e99ada6b3997eeaca4f3c3979a6dad2b
Heather Gray <heathergray153@gmail.com>
2022-04-10 13:50:14 -0600

    'submission Bite 318 @ codechalleng.es'

---
+++ 318/decode.py
@@ -0,0 +1,16 @@
+import base64
+import csv
+from typing import List  # will remove with 3.9
+
+
+def get_credit_cards(data: bytes) -> List[str]:
+    """Decode the base64 encoded data which gives you a csv
+    of "first_name,last_name,credit_card", from which you have
+    to extract the credit card numbers.
+    """
+    decoded_data = base64.b64decode(data)
+    decoded_data = decoded_data.decode('utf-8')
+    cc = []
+    for line in decoded_data.splitlines()[1:]:
+        cc.append(line.split(',')[-1])
+    return cc
