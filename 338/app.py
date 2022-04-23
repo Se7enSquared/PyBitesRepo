984c5032d6210b3abb11d132e1313357662b3bf4
Heather Gray <heathergray153@gmail.com>
2022-04-22 18:38:55 -0600

    'submission Bite 338 @ codechalleng.es'

---
+++ 338/app.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
+from pydantic import BaseModel
+
+
+class Food(BaseModel):
+    """Model from Bite 02"""
+
+    id: int
+    name: str
+    serving_size: str
+    kcal_per_serving: int
+    protein_grams: float
+    fibre_grams: float = 0
+
+
+app = FastAPI()
+foods: Dict[int, Food] = {}
+
+
+@app.post("/", status_code=201)
+async def create_food(food: Food):
+    foods[food.id] = food
+    return food
