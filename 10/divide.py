b883a374899cc0cc3eaa5e252e99ab259869c301
Heather Gray <heathergray153@gmail.com>
2022-04-30 10:53:31 -0600

    refactor bite 10

--- 10/divide.py
+++ 10/divide.py
@@ -5,12 +5,11 @@ from decimal import DivisionByZero
 def positive_divide(numerator, denominator):
     try:
         answer = numerator / denominator
-        if answer < 0:
-            raise ValueError
-        if numerator > 0 and denominator < 0 or numerator < 0 and denominator > 0:
-            raise ValueError
-        if type(numerator) not in (int, float) or type(denominator) not in (int, float):
-            raise TypeError
-        return answer
     except ZeroDivisionError:
         return 0
+    except (TypeError, ValueError):
+        raise
+    else:
+        if answer < 0:
+            raise ValueError('cannot be negative')
+        return answer
