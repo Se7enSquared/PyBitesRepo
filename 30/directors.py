b31e46e43e0a46b770cb333ea4397a6a10808b9d
Heather Gray <heathergray153@gmail.com>
2022-05-14 13:25:53 -0600

    submission Bite 30 @ codechalleng.es

--- 30/directors.py
+++ 30/directors.py
@@ -23,21 +23,28 @@ def get_movies_by_director():
     where keys are directors, and values are a list of movies
     use the defined Movie namedtuple"""
     directors = defaultdict(list)
-    print(csv.__file__)
     with open(MOVIE_DATA, "r") as f:
         csv_reader = csv.reader(f, delimiter=",")
+        # skip the header row
+        next(csv_reader)
         for row in csv_reader:
-            directors[row[1]].append(row[11].strip("\xa0"))
-    # for k, v in directors.items():
-    #     if k == 'Sergio Leone':
-    #         print(f'{k}: {v}')
+            director = row[1]
+            title = row[11].strip("\xa0")
+            year = row[23]
+            if year == '' or int(year) < MIN_YEAR:
+                continue
+            score = float(row[25])
+            directors[director].append(Movie(title, year, score))
     return directors
 
 
 def calc_mean_score(movies):
     """Helper method to calculate mean of list of Movie namedtuples,
     round the mean to 1 decimal place"""
-    pass
+    count, sum = len(movies), 0
+    for movie in movies:
+        sum += movie.score
+    return round(sum / count, 1)
 
 
 def get_average_scores(directors):
@@ -45,20 +52,12 @@ def get_average_scores(directors):
     return a list of tuples (director, average_score) ordered by highest
     score in descending order. Only take directors into account
     with >= MIN_MOVIES"""
-
-    # NOTE: I need to figure out how to get the movies scores and the directors together
-    # TOO TIRED to keep going :(
-    directors = get_movies_by_director()
-    scores = defaultdict(list)
-    directors_scores = []
-    with open(MOVIE_DATA, "r") as f:
-        csv_reader = csv.reader(f, delimiter=",")
-        for row in csv_reader:
-            scores[row[11]].append(row[25])
-    merged_dict = directors.update(scores)
-    for director, movie in merged_dict.items():
-        directors_scores.append(director, scores[movie])
-    print(directors_scores)
+    director_scores = []
+    for director, movies  in directors.items():
+        if len(movies) >= MIN_MOVIES:
+            average_score = calc_mean_score(movies)
+            if average_score > 0:
+                director_scores.append((director, average_score))
+    return sorted(director_scores, key = lambda x: x[1], reverse = True)
 
 
-get_average_scores(get_movies_by_director())
