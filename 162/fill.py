f091a317661283ba94fef84b4eaff76ad8b2ff4b
Heather Gray <heathergray153@gmail.com>
2022-05-26 19:17:38 -0600

    submission Bite 162 @ codechalleng.es

---
+++ 162/fill.py
@@ -0,0 +1,7 @@
+HTML_SPACE = '&nbsp;'
+
+
+def prefill_with_character(value, column_length=4, fill_char=HTML_SPACE):
+    """Prepend value with fill_char for given column_length"""
+    return fill_char * (column_length - len(str(value))) + str(value)
+
