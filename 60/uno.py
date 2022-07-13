41ad4e2b4447ece020b7f3f3a4f81f4c58214f27
Heather Gray <heathergray153@gmail.com>
2022-07-12 21:25:27 -0600

    submission Bite 60 @ codechalleng.es

--- 60/uno.py
+++ 60/uno.py
@@ -1,51 +1,31 @@
 from collections import namedtuple
 
-SUITS = 'Red Green Yellow Blue'.split()
+SUITS = "Red Green Yellow Blue".split()
 
-UnoCard = namedtuple('UnoCard', 'suit name')
+UnoCard = namedtuple("UnoCard", "suit name")
 
 
 def create_uno_deck():
     """Create a deck of 108 Uno cards.
-       Return a list of UnoCard namedtuples
-       (for cards w/o suit use None in the namedtuple)"""
+    Return a list of UnoCard namedtuples
+    (for cards w/o suit use None in the namedtuple)"""
 
     uno_deck = []
 
     for suit in SUITS:
-        uno_deck.append(UnoCard(suit, '0'))
-        uno_deck.append(UnoCard(suit, '1'))
-        uno_deck.append(UnoCard(suit, '1'))
-        uno_deck.append(UnoCard(suit, '2'))
-        uno_deck.append(UnoCard(suit, '2'))
-        uno_deck.append(UnoCard(suit, '3'))
-        uno_deck.append(UnoCard(suit, '3'))
-        uno_deck.append(UnoCard(suit, '4'))
-        uno_deck.append(UnoCard(suit, '4'))
-        uno_deck.append(UnoCard(suit, '5'))
-        uno_deck.append(UnoCard(suit, '5'))
-        uno_deck.append(UnoCard(suit, '6'))
-        uno_deck.append(UnoCard(suit, '6'))
-        uno_deck.append(UnoCard(suit, '7'))
-        uno_deck.append(UnoCard(suit, '7'))
-        uno_deck.append(UnoCard(suit, '8'))
-        uno_deck.append(UnoCard(suit, '8'))
-        uno_deck.append(UnoCard(suit, '9'))
-        uno_deck.append(UnoCard(suit, '9'))
-        uno_deck.append(UnoCard(suit, 'Draw Two'))
-        uno_deck.append(UnoCard(suit, 'Draw Two'))
-        uno_deck.append(UnoCard(suit, 'Skip'))
-        uno_deck.append(UnoCard(suit, 'Skip'))
-        uno_deck.append(UnoCard(suit, 'Reverse'))
-        uno_deck.append(UnoCard(suit, 'Reverse'))
+        uno_deck.append(UnoCard(suit, "0"))
 
-    uno_deck.append(UnoCard(None, 'Wild'))
-    uno_deck.append(UnoCard(None, 'Wild'))
-    uno_deck.append(UnoCard(None, 'Wild'))
-    uno_deck.append(UnoCard(None, 'Wild'))
-    uno_deck.append(UnoCard(None, 'Wild Draw Four'))
-    uno_deck.append(UnoCard(None, 'Wild Draw Four'))
-    uno_deck.append(UnoCard(None, 'Wild Draw Four'))
-    uno_deck.append(UnoCard(None, 'Wild Draw Four'))
+        for i in range(1, 10):
+            uno_deck.extend((UnoCard(suit, str(i)), UnoCard(suit, str(i))))
+        for _ in range(2):
+            uno_deck.extend(
+                (
+                    UnoCard(suit, "Draw Two"),
+                    UnoCard(suit, "Skip"),
+                    UnoCard(suit, "Reverse"),
+                )
+            )
 
+    for _ in range(4):
+        uno_deck.extend((UnoCard(None, "Wild"), UnoCard(None, "Wild Draw Four")))
     return uno_deck
